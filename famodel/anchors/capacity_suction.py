cc4140d8eca801a6dec02c76e363f6accd229363
Moreno <fmoreno@nrel.gov>
2024-05-15 16:25:00 -0600

    Change the suction pile holding methodology to follow S. Kay reserach

--- famodel/anchors/capacity_suction.py
+++ famodel/anchors/capacity_suction.py
@@ -1,218 +1,205 @@
-"""Suction caisson anchor capacity calculation functions in 'level 2' model, 
-calculating inclined load capacity over the range of angles.
-Lead author: Ahmed Radwan. 
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 15:53:52 2024
+
+@author: fmoreno
 """
 
+import yaml      # Allow access to config file for user inputs
 import numpy as np
 import matplotlib.pyplot as plt
-    
+from scipy.optimize import fsolve
+from capacity_load import getAnchorLoad
 
-def getCapacitySuction(L,L_D_aspect=5 ,D_t_aspect=100, A_angle=60, F_angle= 2, 
-                   soil_type='clay', gamma=0, Phi=30, So0= 2.39, k=1.41, Alpha=0.7, J=0.5):
+def getCapacitySuction(D,L,thetalug,psilug,Su0,dSu,gamma, 
+                    alphao,alphai,Np,nhu,Ab,rhows):
     '''Calculate the inclined load capacity of a Suction Caisson Anchor in sand or clay.
     The calculation is based on the soil properties, anchor geometry, and the angle of inclined load.  
     
     Parameters
     ----------
+    D : float 
+        Suction pile diameter [m]
     L : float 
-        Suction anchor height [m]
-    L_D_aspect : float 
-        The aspect ratio of the anchor height and its outer diameter, default is 5.
-    D_t_aspect : float 
-        The aspect ratio of the anchor outer diameter and its thickness, default is 100.
-    A_angle : float 
-        The angle of the tension force at the padeye, default is 60. [deg] 
-    F_angle : float 
-        The adjustment factor of the load angle at the padeye, default is 2. [deg] 
-    soil_type : string
-        Specify 'sand' or 'clay'. This affects what other soil parameters are used.
-    Phi : float
-        The friction angle of the sand soil (sand only), default is 30. [deg] 
-    gamma: float 
-        The effective unit weight of the sand soil, default is 9 for sand, 4.7 for clay. [kN/m3] 
-    J: float 
-        The API- Factor [---], default is 0.5. (sand only)
+        Suction anchor length [m]
+    thetalug : float 
+        Angle of tilt misaligment, default is 5. [deg]
+    psilug : float 
+        Angle of twist misaligment, default is 7.5. [deg] 
     Su0 : float 
-        The Undrained shear strength at the mudline (clay only), default is 2.39. [kPa]
-    k : float 
-        The Undrained shear strength gradient (clay only), default is 1.41. [kPa/m]
-    Alpha : float
-        The skin friction coefficient (clay only), default is 0.7. 
+        Undrained shear strength at the mudline (clay only), default is 2.39. [kPa]
+    dSu : float 
+        Undrained shear strength gradient (clay only), default is 1.41. [kPa/m]
+    gamma: float 
+        Effective unit weight of the sand soil, default is 9 for sand, 4.7 for clay. [kN/m3] 
+    alphao : float 
+        Skin friction coefficient (outer), default is 0.7 [-] 
+    alphai : float
+        Skin friction coefficient (inner), default is 0.7 [-]
+    Np : float
+        Friction angle of the sand soil (sand only), default is 30. [deg]  
+    nhu : float 
+        The API- Factor [---], default is 0.5. (sand only)  
+    rhows : float
+        Submerged steel density [t/m3]
     
     Returns
     -------
-    Ft_values : flaot 
-        The inlcind force capacity at the padeye [kN]
-    Anchor_vol : floar 
-        The corresponding anchor steel volume [m3]
+    Hmax : float 
+        Maximum horizontal capacity [kN]
+    Vmax : float 
+        Maximum vertical capacity [kN]
     '''
     
-    # some constants
-    specific_weight_steel = 78.5 # kN/m^3
-    specific_weight_water = 9.81 # kN/m^3
-    effective_weight_steel = specific_weight_steel - specific_weight_water
+    lambdap = L/D; m = -2/3;
+    t = 10*D/1e3          # Thickness of the pile
+    Nc = min (6.2*(1 + 0.34*np.arctan(lambdap)),9)
+    ez = (Su0*L**2/2 + dSu*L**3/3)/(Su0*L + dSu*L**2/2)
+    Np_fixed = 10.5; Np_free = 4 # From Np vs L/D chart from CAISSON_VHM
+    Su_av_L = Su0 + dSu*L/3; Su_tip = Su0 + dSu*L
+    zlug = ez           # Optimized depth of the lug 
+    rlug = D/2          # Radial position of the lug
     
-    # handle default gamma values
-    if gamma==0:
-        if soil_type=='clay':
-            gamma = 4.7
-        elif soil_type=='sand':
-            gamma = 9
-            
-    # Suction Caisson Anchor Dimensions'
-    D = round((L/L_D_aspect),2)                          # The diameter of the Anchor [m]
-    t =  round(D/D_t_aspect, 3)                           # The thickness of the wall [m]
-    Di = D - 2*t
-    outer_r = D/2
-    inner_r = (D - 2*t)/2
-    height = L
-    outer_vol = np.pi*outer_r**2*height
-    inner_vol = np.pi*inner_r**2*height
-    Anchor_vol = round(outer_vol - inner_vol,2)
+    # Outer and inner surface of the pile skirt
+    def PileSurface(Len,Dia):
+        Sp = np.pi*Dia*Len
+        return Sp    
+    # Dry and wet mass of the pile    
+    def PileWeight(Len,Dia,tw,rho):
+        Wp = ((np.pi/4)*(Dia**2-(Dia-tw)**2)*Len + np.pi*Dia**2*tw)*rho
+        return Wp 
+    # Mass of the soil plug      
+    def SoilWeight(Len,Dia,gamma_soil): 
+        Wsoil =(np.pi/4)*Dia**2*Len*gamma_soil
+        return Wsoil
+    # Tilt and twist effects due to installation misaligments
+    def rlugTilt(r,z,theta):
+        R = r*np.cos(np.deg2rad(theta)) - z*np.sin(np.deg2rad(theta))
+        return R   
+    def zlugTilt(r,z,theta):
+        Z = r*np.sin(np.deg2rad(theta)) + z*np.cos(np.deg2rad(theta))
+        return Z
+           
+    Hmax = Np_fixed*L*D*Su_av_L; H0 = Np_free*L*D*Su_av_L;
+    Mmax = Np_fixed*L*L*D*Su_av_L; print(Hmax)
     
-    # ----- clay case -----
-    if soil_type == 'clay':
+    resultsLoad = getAnchorLoad(Tm,thetam,zlug,Su0,dSu,nhu,Ab,Nc)
+    M = - resultsLoad['V']*rlugTilt(rlug,zlug,thetalug) - resultsLoad['H']*(zlugTilt(rlug,zlug,thetalug) - ez)
     
-        Li_percent = 0.67     # The pedeye location. 
-        Li= L*Li_percent
-        F_angle = 1           # NOTE: this is a hardcoded override of the input parameter <<<
+    # M modifies the Hmax capacity
+    def f(Hmax):
+         return m*(Hmax/(L*D*(Su0 + dSu*L/3)) - Np_fixed) + M*(Hmax/(L*D*(Su0 + dSu*L/3))/(Hmax*L))
+    Hmax = fsolve(f,5); print(Hmax[0])
     
-
-        # Calculations related to the overburben pressure
-        Lo_min = 0.1
-        Lo_incr = 3
-        N_inc = 34
-        Lo = np.arange(Lo_min, Lo_incr*N_inc ,Lo_incr)
-        F = abs(1 - (Li/Lo))
-        n =100.00
-        delta = L/n           # The increment of the soil profile. 
-        inc_Z = L/100
-        Z = np.arange(0.075, L, inc_Z)
-        #fb=1.35
-        
-        # The calculations of the vertical capacity
-        Su = So0 + k*Z
-        Su_av = np.mean(Su)
-        V = np.pi*D*L*Su_av*Alpha + 9*Su[-1]*np.pi*((D**2) /4)
-        
-        # The calculations of the horizontal capacity
-        Np = 3 + gamma/Su + Z*(J/D)          # calculate Np element-wise
-        Np = np.where(Np < 9, Np, 9)         # replace any values greater than 9 with 9
-        Pult_D= Np * Su  
-        VX_mat= np.zeros((len(Z), len(Lo)))
-        for i in range(len(Lo)):
-            VX_mat[:,i]=abs(1-(Z/Lo[i]))   
-        dE_mat= np.zeros((len(Z), len(Lo)))  
-        for i in range(len(Z)-1):
-            for j in range(len(Lo)):
-                dE_mat[i+1, j] = Pult_D[i+1]*delta*D*VX_mat[i+1, j] + dE_mat[i, j]        
-        H = dE_mat[-1,:] / F
-        Hm = min(H) #*fb
-
+    Fo = PileSurface(L,D)*alphao*Su_av_L
+    T = resultsLoad['H']*rlug*np.sin(np.deg2rad(psilug))
+    To = PileSurface(L,D)*alphao*Su_av_L
+    Ti = PileSurface(L,(D -2*t))*alphai*Su_av_L
+    Tbase = np.pi*D**3*Su_tip/12
+    Tmax = min(Ti + To,To + Tbase)
     
-    # ----- sand case -----
-    elif soil_type == 'sand':
-       
-        # The calculations of the Vertical capacity
-        ktans = 0.5
-        Zo = D/(4*ktans)
-        Zi = Di/(4*ktans)
-        Xo = L/Zo
-        Xi = L/Zi
-        Yo = np.exp(-Xo) - 1 + Xo
-        Yi = np.exp(-Xi) - 1 + Xi
-        V = gamma*(Zo**2)*Yo*ktans*np.pi*D + gamma*(Zi**2)*Yi*ktans*np.pi*Di 
-
-        # The calculations of the horizontal capacity
-        Nq = np.exp(np.pi*np.tan(np.radians(Phi)))*np.tan(np.radians(45 + (Phi/2)))**2
-        Hm = 0.5*D*Nq*gamma*L**2
-
-    else:
-        raise Exception(f"Unsupported soil_type '{soil_type}'. Must be sand or clay.")
+    # Introduce twist effects
+    nhuT = T/Tmax; nhuV = resultsLoad['H']/Fo;
+    nhuVstar = np.sqrt(nhuV**2 - nhuT**2)
+    alphaostar = alphao*(nhuVstar/nhuV)
     
+    Vmax1 = PileWeight(L,D,t,rhows) + PileSurface(L,D)*alphaostar*Su_av_L + Nc*Su_tip*np.pi*D**2                    # "Plugged" (Reverse end bearing capacity) 
+    Vmax2 = PileWeight(L,D,t,rhows) + PileSurface(L,D)*alphaostar*Su_av_L + PileSurface(L,(D - 2*t))*alphai*Su_av_L # "Coring"
+    Vmax3 = PileWeight(L,D,t,rhows) + PileSurface(L,D)*alphaostar*Su_av_L + SoilWeight(L,D,gamma)                   # "Leaking"
+    Vmax = min(Vmax1,Vmax2,Vmax3)
     
-    # ----- add weight to total vertical capacity -----
-    Vm = V + Anchor_vol*effective_weight_steel
-        
-        
-    # ----- Interaction Curve (same for clay and sand) -----
+    print('Hmax = ' +str(Hmax[0]) +' kN')
+    print('Vmax = ' +str(Vmax) +' kN')
+    print('Tmax = ' +str(Tmax) +' kN*m')
     
-    # Define the paramaters
-    # Hm = Hmin   <<< removing because we can just use consistent variable names instead
-    # Vm = Vm
+    Wp = 1.10*PileWeight(L,D,t,rhows)  
     
-    # Define the exponents
-    a = 0.5 + L/D
-    b = 4.5 + L/(3*D)
-    
-    # Define the function (for holding capacity?)
-    def f(h, v):
-        return (h/Hm)**a + (v/Vm)**b
-    
-    # Create a grid of horizontal and vertical load values
-    h = np.linspace(0, Hm, 100)    # NOTE: what is the best choice for these values (clay was 100, sand was 2000) <<<
-    v = np.linspace(0, Vm, 100)
-    h_grid, v_grid = np.meshgrid(h, v)
-    
-    # Calculate the values of f on the grid
-    f_grid = f(h_grid, v_grid)
-    
-    # Find the contour line where f < 1
-    FB = min(0.99, (1**a) + (1**b))
-    #contour_level = FB
-    contour_lines = plt.contour(h_grid, v_grid, f_grid, levels=[FB], colors='b')
-    #contour_lines = plt.contour(h_grid, v_grid, f_grid, colors='b')
+    aVH = 0.5 + lambdap; bVH = 4.5 + lambdap/3 
+    UC = (resultsLoad['H']/Hmax)**aVH + (resultsLoad['V']/Vmax)**bVH      
+    x = np.cos(np.linspace (0,np.pi/2,100))
+    y = (1 - x**bVH)**(1/aVH)
+    X = Hmax*x; Y = Vmax*y
+    plt.plot(X,Y,color = 'b')
+    plt.scatter(resultsLoad['H'],resultsLoad['V'],color = 'r')
     
     # Set labels and title
     plt.xlabel('Horizontal capacity [kN]')
     plt.ylabel('Vertical capacity [kN]')
     plt.suptitle('VH suction pile capacity envelope')
-    plt.xlim(0,1.3*Hm); plt.ylim(0,1.3*Vm)
+    plt.axis([0,1.3*max(X[0],resultsLoad['H']),0,1.3*max(Y[-1],resultsLoad['V'])]) 
+    plt.grid(True)
+    plt.show()
     
-    # Extract the coordinates of the contour line
-    contour_line = contour_lines.collections[0].get_paths()[0]
-    h_values, v_values = contour_line.vertices[:, 0], contour_line.vertices[:, 1]
+    resultsSuction = {}
+    resultsSuction['Horizontal max.'] = Hmax[0] # Capacity at specified loading angle
+    resultsSuction['Vertical max.'] = Vmax      # Capacity at specified loading angle
+    resultsSuction['UC'] = UC[0]                # Unity check
+    resultsSuction['Weight'] = Wp
+    resultsSuction['t'] = t
     
-    # Estimating the inclined capacity
-    angle = np.degrees(np.arctan(v_values/h_values))
-    Ft  = np.sqrt(h_values**2 + v_values**2); print(Ft)
-    #print(h_values); print(v_values)
-    Ft1 = h_values/np.abs(np.cos(np.radians(angle)))
-    Ft2 = v_values/np.abs(np.sin(np.radians(angle)))
-    #print(Ft1); print(Ft2)
+    return resultsSuction
     
-    # Extract the capacity values at the desired loading angle
-    v_values_target  = np.interp(A_angle + F_angle, angle, v_values)
-    h_values_target  = np.interp(A_angle + F_angle, angle, h_values)
-    Ft_values_target = round(np.interp(A_angle + F_angle, angle, Ft),2)
-    
-    results = {}
-    results['capacity'] = Ft_values_target  # capacity at specified loading angle
-    results['capacity_force'] = Ft          # capacity envelope force magnitude
-    results['capacity_angle'] = angle       # capacity envelope force angle
-    results['vol'] = Anchor_vol
-    results['L'] = L
-    results['D'] = D
-    results['t'] = t
-    results['Anchor_vol'] = Anchor_vol
-    
-    return results
-    
-    
-
 if __name__ == '__main__':
- 
+          
+    ''' 
+    Testing the function 
+    '''
+  
+    # Retrieves input data from a separate config file
+    with open('SuctionConfig.yml', 'r') as f:
+        configSuction = yaml.full_load(f)
+    D = configSuction['D']; L = configSuction['L'];  
+    thetalug = configSuction['thetalug']; psilug = configSuction['psilug'];
+    Su0 = configSuction['Su0']; dSu = configSuction['dSu']; gamma = configSuction['gamma']
+    alphao = configSuction['alphao']; alphai = configSuction['alphai']
+    Np = configSuction['Np']; Nc = configSuction['Nc']; Ab = configSuction['Ab']
+    nhu = configSuction['nhu']; rhows = configSuction['rhows']
     
-    ''' Testing the function in one case of the anchor with length = 15 m, 
-    the aspect ratio of the anchor height and its outer diameter, default is 3, all other parameters are default.'''
- 
-    results = getCapacitySuction(15, L_D_aspect=5, soil_type='sand')
-    print('********************* One Case Test Result********************')
+    # Retrieves input data from a separate config file
+    with open('LoadConfig.yml', 'r') as f:
+        configLoad = yaml.full_load(f)
+    Tm = configLoad['Tm']; thetam = configLoad['thetam']
+    zlug = configLoad['zlug']; 
+    Su0 = configLoad['Su0']; dSu = configLoad['dSu']
+    nhu = configLoad['nhu']; 
+    Nc = configLoad['Nc']; Ab = configLoad['Ab']  
+    
+    resultsSuction = getCapacitySuction(D,L,thetalug,psilug,Su0,dSu,gamma, 
+                        alphao,alphai,Np,nhu,Ab,rhows)
+    
+    print('******************  Suction Pile Result  *********************')
 
-    print('Anchor length,              ' , results['L'], '[m]') 
-    print('Anchor diameter,            ' , results['D'], '[m]') 
-    print('Anchor thickness,           ' , results['t'], '[m]')
-    print('Anchor steel volume,        ' , results['Anchor_vol'], '[m3]') 
-    print('Inclined load capacity,     ' , results['capacity'], '[kN]') 
+    print('Anchor thickness,                    ' , resultsSuction['t'], '[m]')
+    print('Anchor steel weight,                 ' , resultsSuction['Weight'], '[t]') 
+    print('Horizontal max. capacity,            ' , resultsSuction['Horizontal max.'], '[kN]')
+    print('Vertical max. capacity,              ' , resultsSuction['Vertical max.'], '[kN]') 
+    print('Unity check capacity,                ' , resultsSuction['UC'], '[-]') 
 
     print('**************************************************************') 
+     
+    # delta_phiMH = 2.5 - 0.25*lambdap  # Formula in deg, depends on ez_L
+    # phiMH = -np.arctan(ez_L) - np.deg2rad(delta_phiMH)
+    # aMH = np.cos(phiMH)/Np_fixed; bMH = Np_free*np.sin(phiMH)
+    # print(aMH); print(bMH)
+    
+    # N = 50
+    # alpha = np.linspace(0,2*np.pi,N)
+    # phi = np.linspace(0,np.pi/2,N)
+    # Alpha, Phi = np.meshgrid(alpha,phi)
+    
+    # x = (aMH*np.cos(phiMH)*np.cos(Alpha) - bMH*np.sin(phiMH)*np.sin(Alpha))*np.sin(Phi)
+    # y = (aMH*np.sin(phiMH)*np.cos(Alpha) + bMH*np.cos(phiMH)*np.sin(Alpha))*np.sin(Phi)
+    # z = ((1-np.abs(1/Vmax))**bVH)**(2/aVH)*np.cos(Phi)
+    
+    # # First subplot
+    # fig = plt.figure(figsize=(15,20))
+    # ax = fig.add_subplot(3,1,1,projection='3d')
+    # surf = ax.plot_surface(x,y,z,cmap=cm.coolwarm)
+    # load = ax.scatter(H/(D*L*(Su0+dSu*L/3)), M/(D*L*L*(Su0+dSu*L/3)), V/Vmax, color = 'r')
+    
+    # # Set labels and title
+    # ax.set_xlabel('H* (H/(D*L*Su,av,L)) [-]')
+    # ax.set_ylabel('M* (M/(D*L^2*Su,av,L)) [-]')
+    # ax.set_zlabel('V [-]')
+    # ax.set_title('VHM Ellipsoid suction pile capacity')
+    # plt.show()
